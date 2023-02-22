b0e75c86f901af1fdcf7169c5b3a5d933607940e
Gratien Dhaese <gratien.dhaese@gmail.com>
2023-02-22 11:59:04 +0100

    adding pi4-uptime-kuma-helm

---
+++ container/build.sh
@@ -0,0 +1,9 @@
+# Build the pi4-uptime-kuma container image
+# To build with a new version: ./build.sh v1.1
+REL=${1:-v1.0}
+cat ~/.ghcr-token | docker login ghcr.io -u gdha --password-stdin
+echo "Building pi4-uptime-kuma:$REL"
+docker build --tag ghcr.io/gdha/pi4-uptime-kuma:$REL .
+docker tag ghcr.io/gdha/pi4-uptime-kuma:$REL ghcr.io/gdha/pi4-uptime-kuma:latest
+echo "Pushing pi4-uptime-kuma:$REL to GitHub Docker Container registry"
+docker push ghcr.io/gdha/pi4-uptime-kuma:$REL
